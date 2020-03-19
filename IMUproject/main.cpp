ecac47343c47389a306d6a74d83fcbcb5036c33d
Martin Skårerverket <marskaar@ntnu.no>
2020-03-19 15:02:44 +0100

    source

---
+++ IMUproject/main.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include "LSM9DS1.h"
+
+using namespace std;
+
+
+int main(int argc, char* argv[]) {
+
+
+	
+
+	// Accelerometer Test
+
+	LSM9DS1 accelerometer(2, 0x6b);
+	accelerometer.setRange(PLUSMINUS_2g);
+	accelerometer.setODR(XL_ODR_952);
+
+	cout << "The current ODR is: " << (int)accelerometer.getODR() << endl;
+	cout << "The current range is: " << (int)accelerometer.getRange() << endl;
+
+	while (1 < 2) {
+
+		accelerometer.readFullSensorState();
+		int x = accelerometer.getAccelerationX();
+		int y = accelerometer.getAccelerationY();
+		int z = accelerometer.getAccelerationZ();
+		cout << "Current Acceleration: " << x << "," << y << "," << z << endl;
+
+	}
+
+
+
+
+	return 0;
+}
